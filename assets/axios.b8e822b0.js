fd7055d6025b5658f9bb60be60b339c08c08c774
NailShort <fable014101@gmail.com>
2023-03-01 06:06:30 +0000

    Deploying to gh-pages from @ NailShort/Ocean-front@24babaf668155ea9e158b88ee6a777e39b374d1c 🚀

---
+++ assets/axios.b8e822b0.js
@@ -0,0 +1,1 @@
+import{f as e,j as a}from"./index.7857a1ea.js";const r=a.create({baseURL:"https://ocean-back.onrender.com"});var i=e(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=r});export{r as api,i as default};
